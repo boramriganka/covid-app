fd6c726b584e9975c9dca834f2c7d839e44e08fa
boramriganka <42547729+boramriganka@users.noreply.github.com>
2020-11-16 17:03:21 +0530

    Update Chart.js

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api/api';
 import { Line, Bar, Pie} from 'react-chartjs-2';
 import styles from './Chart.module.css';
-import axios from 'axios';
 
 const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 	const [dailyData, setDailyData] = useState([]);
@@ -54,40 +53,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 		/>
 	) : null;
 
-	const barChart = confirmed ? (
-		<Bar
-			data={{
-				labels: ['Infected', 'Recovered', 'Deaths', 'Active'],
-				datasets: [
-					{
-						label: 'People',
-						backgroundColor: [
-							'rgba(0, 0, 255, 0.5)',
-							'rgba(0, 255, 0, 0.5)',
-							'rgba(255, 0, 0, 0.5)',
-							'rgba(242, 234, 0, 0.5)',
-						],
-						hoverBackgroundColor: [
-							'rgba(0, 77, 153)',
-							'rgba(30, 102, 49)',
-							'rgba(255, 51, 51)',
-							'rgba(204, 153, 0)',
-						],
-						data: [
-							confirmed.value,
-							recovered.value,
-							deaths.value,
-							confirmed.value - (recovered.value + deaths.value),
-						],
-					},
-				],
-			}}
-			options={{
-				legend: { display: false },
-				title: { display: true, text: `Current state in ${country}` },
-			}}
-		/>
-  ) : null;
+	
   
 	const pieChart = confirmed ? (
 		<Pie
