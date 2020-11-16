553a215a0f20e0444d3aa6dfd14a7d9e6c73849e
boramriganka <42547729+boramriganka@users.noreply.github.com>
2020-11-16 16:48:11 +0530

    Create Container.js

---
+++ src/Container.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './container.css';
+import App from './App';
+function Header() {
+    const [ darkMode, setDarkMode ] = React.useState(false)
+     
+    React.useEffect(() => {
+      const body = document.body
+      const toggle = document.querySelector('.toggle-inner')
+      
+      // If dark mode is enabled - adds classes to update dark-mode styling.
+      // Else, removes and styling is as normal.
+      if( darkMode === true ) {
+        body.classList.add('dark-mode')
+        toggle.classList.add('toggle-active')
+      } else {
+        body.classList.remove('dark-mode')
+        toggle.classList.remove('toggle-active')
+      }
+    }, [darkMode])
+    
+    return (
+      <header>
+        <div
+          id="toggle"
+          onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)}
+        >
+          <div className="toggle-inner"/>
+          
+        </div>
+        <span>dark/Light</span>
+      </header>
+    )
+  }
+  
+  function Container() {
+    return (
+        <>
+      <main>
+        <Header />
+        <div id="container">
+          <App/>
+        </div>
+      </main>
+      </>
+    )
+  }
+  
+ export default Container;
